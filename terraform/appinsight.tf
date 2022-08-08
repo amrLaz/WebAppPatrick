680014bac2c7f48726454c8d643418a26ea5a56b
Amr LAZRAQ <amr.lazraq@exakis-nelite.com>
2022-08-08 17:03:30 +0200

    fix: change appinsight application_type

--- terraform/appinsight.tf
+++ terraform/appinsight.tf
@@ -15,7 +15,7 @@ resource "azurerm_application_insights" "appinsight" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   workspace_id        = azurerm_log_analytics_workspace.workspace.id
-  application_type    = "web"
+  application_type    = "other"
 }
 
 resource "azurerm_application_insights_web_test" "appinsightwebtest" {
